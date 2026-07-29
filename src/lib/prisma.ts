5d41ae1b27038797323c210ba16ae011e4e54e15
Reymartdulla <152572094+Reymartdulla@users.noreply.github.com>
2026-07-29 23:17:08 +0800

    Fix Prisma Client pooler connection for Vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,10 +2,19 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+const getDatabaseUrl = () => {
+  let url = process.env.DATABASE_URL;
+  if (url && url.includes('-pooler.') && !url.includes('pgbouncer=true')) {
+    url += url.includes('?') ? '&pgbouncer=true' : '?pgbouncer=true';
+  }
+  return url;
+};
+
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ['query'],
+    datasourceUrl: getDatabaseUrl(),
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
